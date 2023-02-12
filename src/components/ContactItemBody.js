9b078ccc9b1e7157fb0b2cfcb1a571cd59fb3193
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-12 22:09:17 +0700

    PropTypes

--- src/components/ContactItemBody.js
+++ src/components/ContactItemBody.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ContactItemBody({ name, tag }) {
     return (
@@ -7,6 +8,11 @@ function ContactItemBody({ name, tag }) {
             <p className="contact-item__username">@{tag}</p>
         </div>
     );
+}
+
+ContactItemBody.propTypes = {
+    name: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired
 }
 
 export default ContactItemBody;
