c72fd2d28dd15fe35ef410c3df57477c4af811b7
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-12 22:07:50 +0700

    PropTypes

--- src/components/ContactInput.js
+++ src/components/ContactInput.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class ContactInput extends React.Component {
     constructor(props) {
@@ -45,6 +46,10 @@ class ContactInput extends React.Component {
             </form>
         )
     }
+}
+
+ContactInput.propTypes = {
+    addContact: PropTypes.func.isRequired,
 }
 
 export default ContactInput;
