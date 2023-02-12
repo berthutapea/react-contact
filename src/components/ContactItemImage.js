52a686ee92875fa8a6ff0a955427c69a5adbcc58
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-12 22:09:55 +0700

    PropTypes

--- src/components/ContactItemImage.js
+++ src/components/ContactItemImage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 function ContactItemImage({ imageUrl }) {
     return (
@@ -6,6 +7,10 @@ function ContactItemImage({ imageUrl }) {
             <img src={imageUrl} alt="contact avatar" />
         </div>
     );
+}
+
+ContactItemImage.propTypes = {
+    imageUrl: PropTypes.string.isRequired,
 }
 
 export default ContactItemImage;
