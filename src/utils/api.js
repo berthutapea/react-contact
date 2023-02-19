e23623499485d5d7084edcc478d99c73243ce48a
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-20 03:45:24 +0700

    api

--- src/utils/api.js
+++ src/utils/api.js
@@ -17,3 +17,22 @@ async function fetchWithToken(url, options = {}) {
         }
     });
 }
+
+async function login({ email, password}) {
+    const response = await fetch(`${BASE_URL}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password }),
+    });
+
+    const responseJson = await response.json();
+
+    if (responseJson.status !== 'success') {
+        alert(responseJson.message);
+        return { error: true, data : null };
+    }
+
+    return { error: false, data: responseJson.data };
+}
