5384f71638cb8832369af3701be48c10bdfb0d31
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-20 05:15:59 +0700

    Login

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -4,6 +4,8 @@ import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
 import RegisterPage from '../pages/RegisterPage';
+import LoginPage from '../pages/LoginPage';
+import { getUserLogged, putAccessToken } from '../utils/api';
 
 
 class ContactApp extends React.Component {
@@ -13,6 +15,19 @@ class ContactApp extends React.Component {
         this.state = {
             authedUser: null,
         };
+
+        this.onLoginSuccess = this.onLoginSuccess.bind(this);
+    }
+
+    async onLoginSuccess({ accessToken }) {
+        putAccessToken(accessToken);
+        const { data } = await getUserLogged();
+
+        this.setState(() => {
+            return {
+                authedUser: data,
+            };
+        });
     }
 
     render() {
@@ -25,7 +40,7 @@ class ContactApp extends React.Component {
                     </header>
                     <main>
                         <Routes>
-                            <Route path="/*" element={<p>Halaman Login</p>} />
+                            <Route path="/*" element={<LoginPage loginSuccess={this.onLoginSuccess} />} />
                             <Route path="/register" element={<RegisterPage />} />
                         </Routes>
                     </main>
