7c935c9b7ac76902975027233be644b37b61e944
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-20 04:22:10 +0700

    ContactApp

--- src/components/ContactApp.js
+++ src/components/ContactApp.js
@@ -3,70 +3,49 @@ import { Route, Routes } from 'react-router-dom';
 import Navigation from './Navigation';
 import HomePage from '../pages/HomePage';
 import AddPage from '../pages/AddPage';
-// import ContactList from './ContactList';
-// import { getContacts, getData } from '../utils/data';
-// import ContactInput from './ContactInput';
 
-function ContactApp() {
-    return (
-        <div className="contact-app">
-            <header className='contact-app__header'>
-                <h1>Aplikasi Kontak</h1>
-                <Navigation />
-            </header>
-            <main>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/add" element={<AddPage />} />
-                </Routes>
-            </main>
-        </div>
-    );
+class ContactApp extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            authedUser: null,
+        };
+    }
+
+    render() {
+
+        if (this.state.authedUser === null) {
+            return (
+                <div className='contact-app'>
+                    <header className='contact-app__header'>
+                        <h1>Aplikasi Kontak</h1>
+                    </header>
+                    <main>
+                        <Routes>
+                            <Route path="/*" element={<p>Hlaman Login</p>} />
+                            <Route path="/register" element={<p>Hlaman Register</p>} />
+                        </Routes>
+                    </main>
+                </div>
+            )
+        }
+
+        return (
+            <div className="contact-app">
+                <header className='contact-app__header'>
+                    <h1>Aplikasi Kontak</h1>
+                    <Navigation />
+                </header>
+                <main>
+                    <Routes>
+                        <Route path="/" element={<HomePage />} />
+                        <Route path="/add" element={<AddPage />} />
+                    </Routes>
+                </main>
+            </div>
+        );
+    }
 }
-
-// class ContactApp extends React.Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             contacts: getContacts(),
-//         }
-
-//         this.onDeleteHandler = this.onDeleteHandler.bind(this);
-//         this.onAddContactHandler = this.onAddContactHandler.bind(this);
-//     }
-
-//     onDeleteHandler(id) {
-//         const contacts = this.state.contacts.filter(contact => contact.id !== id);
-//         this.setState({ contacts });
-//     }
-
-//     onAddContactHandler({ name, tag }) {
-//         this.setState((prevState) => {
-//             return {
-//                 contacts: [
-//                     ...prevState.contacts,
-//                     {
-//                         id: +new Date(),
-//                         name,
-//                         tag,
-//                         imageUrl: '/images/default.jpg',
-//                     }
-//                 ]
-//             }
-//         });
-//     }
-
-//     render() {
-//         return (
-//             <div className="contact-app">
-//                 <h1>Aplikasi Kontak</h1>
-//                 <h2>Tambah Kontak</h2>
-//                 <ContactInput addContact={this.onAddContactHandler} />
-//                 <h2>Daftar Kontak</h2>
-//                 <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} />
-//             </div>
-//         );
-//     }
-// }
 
 export default ContactApp;
