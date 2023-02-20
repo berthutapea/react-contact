2f8a32479acd9a2b89c04aa08dbeceeb23b3da0b
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-21 02:31:04 +0700

    Locale Consumer

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,22 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { FiHome, FiPlusCircle, FiLogOut } from 'react-icons/fi';
+import { LocaleConsumer } from '../contexts/LocaleContext';
+
 
 function Navigation({ logout, name }) {
     return (
-        <nav className="navigation">
-            <ul>
-                <li><Link to="/"><FiHome /></Link></li>
-                <li><Link to="/add"><FiPlusCircle /></Link></li>
-                <li><button onClick={logout}>{name} <FiLogOut /></button></li>
-            </ul>
-        </nav>
+        <LocaleConsumer>
+            {
+                ({ locale, toggleLocale }) => {
+                    return (
+                        <nav className="navigation">
+                            <ul>
+                                <li><Link to="/"><FiHome /></Link></li>
+                                <li><Link to="/add"><FiPlusCircle /></Link></li>
+                                <li><button onClick={logout}>{name} <FiLogOut /></button></li>
+                            </ul>
+                        </nav>
+                    )
+                }
+            }
+        </LocaleConsumer>
     );
 }
 
+
 Navigation.propTypes = {
     logout: PropTypes.func.isRequired,
-    name: PropTypes.string.isRequired
+    name: PropTypes.string.isRequired,
 };
+
 
 export default Navigation;
