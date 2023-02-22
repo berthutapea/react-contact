d796408cfa47f5f14e492f837dd070e3c3484ef8
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-22 20:35:19 +0700

    Delete

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { FiDelete } from 'react-icons/fi';
+// import React from 'react';
+// import PropTypes from 'prop-types';
+// import { FiDelete } from 'react-icons/fi';
 
-function DeleteButton({ id, onDelete }) {
-    return <button className='contact-item__delete' onClick={() => onDelete(id)}><FiDelete /></button>
-}
+// function DeleteButton({ id, onDelete }) {
+//     return <button className='contact-item__delete' onClick={() => onDelete(id)}><FiDelete /></button>
+// }
 
-DeleteButton.propTypes = {
-    id: PropTypes.string.isRequired,
-    onDelete: PropTypes.func.isRequired,
-}
+// DeleteButton.propTypes = {
+//     id: PropTypes.string.isRequired,
+//     onDelete: PropTypes.func.isRequired,
+// }
 
-export default DeleteButton;
+// export default DeleteButton;
