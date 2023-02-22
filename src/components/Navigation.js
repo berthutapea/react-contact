f0f37a4559a983bfb3bc0907acaafdbbf084038c
msnproduction <111676859+berthutapea@users.noreply.github.com>
2023-02-22 20:36:47 +0700

    Navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,36 +1,36 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom';
-// import { FiHome, FiPlusCircle, FiLogOut } from 'react-icons/fi';
-// import { LocaleConsumer } from '../contexts/LocaleContext';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { FiHome, FiPlusCircle, FiLogOut } from 'react-icons/fi';
+import { LocaleConsumer } from '../contexts/LocaleContext';
 
 
-// function Navigation({ logout, name }) {
-//     return (
-//         <LocaleConsumer>
-//             {
-//                 ({ locale, toggleLocale }) => {
-//                     return (
-//                         <nav className="navigation">
-//                             <ul>
-//                                 <li><button onClick={toggleLocale}>{locale === 'id' ? 'en' : 'id'}</button></li>
-//                                 <li><Link to="/"><FiHome /></Link></li>
-//                                 <li><Link to="/add"><FiPlusCircle /></Link></li>
-//                                 <li><button onClick={logout}>{name} <FiLogOut /></button></li>
-//                             </ul>
-//                         </nav>
-//                     )
-//                 }
-//             }
-//         </LocaleConsumer>
-//     );
-// }
+function Navigation({ logout, name }) {
+    return (
+        <LocaleConsumer>
+            {
+                ({ locale, toggleLocale }) => {
+                    return (
+                        <nav className="navigation">
+                            <ul>
+                                <li><button onClick={toggleLocale}>{locale === 'id' ? 'en' : 'id'}</button></li>
+                                <li><Link to="/"><FiHome /></Link></li>
+                                <li><Link to="/add"><FiPlusCircle /></Link></li>
+                                <li><button onClick={logout}>{name} <FiLogOut /></button></li>
+                            </ul>
+                        </nav>
+                    )
+                }
+            }
+        </LocaleConsumer>
+    );
+}
 
 
-// Navigation.propTypes = {
-//     logout: PropTypes.func.isRequired,
-//     name: PropTypes.string.isRequired,
-// };
+Navigation.propTypes = {
+    logout: PropTypes.func.isRequired,
+    name: PropTypes.string.isRequired,
+};
 
 
-// export default Navigation;
+export default Navigation;
